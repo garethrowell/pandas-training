a7cea7ed830cd112949362f312c0b976bc72e497
HeartlandNetwork <gareth_rowell@nps.gov>
2024-08-13 10:21:44 -0500

    Ch 5 - Ch 6

---
+++ src/Ch5-Getting started-with-pandas.py
@@ -0,0 +1,69 @@
+
+
+# ----------------------------------------------------
+# Ch 5 Getting started with pandas
+# ----------------------------------------------------
+
+
+# ----------------------------------------------------
+# 5.1 Introduction to pandas data structures
+# ----------------------------------------------------
+
+
+# Series
+# ------
+
+# DataFrame
+# ---------
+
+# Index objects
+#--------------
+
+
+
+# ----------------------------------------------------
+# 5.2 Essential Functionality
+# ----------------------------------------------------
+
+
+# Reindexing
+# ----------
+
+# Dropping Entries from an Axis
+# -----------------------------
+
+
+# Indexing, Selection and Filtering
+# ---------------------------------
+
+# Arithmetic and Data Alignment
+# -----------------------------
+
+# Function Application and Mapping
+# --------------------------------
+
+# Sorting and ranking
+# -------------------
+
+# Axis Indexes with Duplicate labels
+# ----------------------------------
+
+
+
+# ----------------------------------------------------
+# 5.3 Summarizing and Computing Descriptive Statistics
+# ----------------------------------------------------
+
+# Correlation and covariance
+# --------------------------
+
+# Unique values, value couns, and membership
+# ------------------------------------------
+
+# Conclusion
+# ----------
+
+
+
+
+
