f428fc5b96704b1b5321a2f23c588d24199c8375
HeartlandNetwork <gareth_rowell@nps.gov>
2024-09-03 14:17:45 -0500

    Starting Ch3 - tuples

--- src/Ch3-Data-structures-functions-files.py
+++ src/Ch3-Data-structures-functions-files.py
@@ -11,6 +11,102 @@
 # Tuple
 # -----
 
+# creating a tuple
+
+tup = (4, 5, 6)
+
+tup
+
+# you can omit the parentheses
+
+tup2 = 4, 5, 6
+
+tup2
+
+# convert an iterator or sequence to a tuple
+
+
+tuple([4, 0, 2])
+
+tup = tuple('string')
+
+tup
+
+# illustrating the zero index
+
+tup[0]
+
+
+# for more complex expressions, use parentheses
+
+nested_tup = (4, 5, 6), (7, 8)
+
+nested_tup
+
+nested_tup[0]
+
+nested_tup[1]
+
+# immutable nature of tuples
+
+tup = tuple(['foo', [1, 2], True])
+
+tup[2]
+
+tup[2] = False
+
+# If object inside tuple is mutable, you can modify it in place
+
+tup[1].append(3)
+
+tup
+
+# use the '+' sign to concatenate tuples
+
+(4, None, 'foo') + (6, 0) + ('bar',)
+
+# multiply tuple with integer - concatenating multiple copies
+# note the objects aren't copied, just their references
+
+('foo', 'bar') * 4
+
+
+# Unpacking tuples ---------------------
+
+tup = (4, 5, 6)
+
+a, b, c = tup
+
+b
+
+
+# Unpacking sequencs of nested tuples
+
+tup = 4, 5, (6, 7)
+
+a, b, (c, d) = tup
+
+d
+
+# swapping variable names
+# traditional
+
+tmp = a
+
+a = b
+
+b = tmp
+
+# Python approach
+
+a, b = 1, 2 <<<<<<<<<<<<<<<<<
+
+
+
+
+
+
+
 
 # List
 # ----
