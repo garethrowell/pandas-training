ea9a5f5c1f61d754d7b48f29d2e35f21782174ed
HeartlandNetwork <gareth_rowell@nps.gov>
2024-09-04 10:47:06 -0500

    tuples then lists

--- src/Ch3-Data-structures-functions-files.py
+++ src/Ch3-Data-structures-functions-files.py
@@ -99,17 +99,129 @@ b = tmp
 
 # Python approach
 
-a, b = 1, 2 <<<<<<<<<<<<<<<<<
+a, b = 1, 2 
+
+a
+
+b
+
+b, a = a, b
+
+a
+
+b
+
+# more tuple unpacking
+
+seq = [(1, 2, 3), (4, 5, 6), (7, 8, 9)]
+
+for a, b, c in seq:
+    print(f'a={a}, b={b}, c={c}')
+    
+    
+# tuples are also a good way to return multiple values from a functions
+
+# *rest can be used to capture arbitrarily long positional arguments, or to pull out
+# individual arguments as follows
+
+values = 1, 2, 3, 4, 5
+
+a, b, *rest = values
+
+a
+
+b
+
+rest
+
+# alternate syntaz
+
+a, b, *_ = values
 
 
 
+# Tuple methods ---------------------
 
+a = (1, 2, 2, 2, 3, 4, 2)
 
+a.count(2)
 
 
 
 # List
 # ----
+
+# are variable length and can be modified in-place
+
+a_list = [2, 3, 7, None]
+
+tup = ("foo", "bar", "baz")
+
+b_list = list(tup)
+
+b_list
+
+b_list[1] = "peekaboo"
+
+b_list
+
+# Lists and tuples can be used interchangeably in many functions
+
+# materializing an iterator or generator
+
+gen = range(10)
+
+gen
+
+list(gen)
+
+# adding and removing elements
+
+b_list.append("dwarf")
+
+b_list
+
+b_list.insert(1, "red")
+
+b_list
+
+# insert is computationally expensive compared to append
+# consider collections.deque
+
+# using pop
+
+b_list.pop(2)
+
+b_list
+
+# remove() takes off the first occurrence of the values
+
+b_list.append("foo")
+
+b_list
+
+b_list.remove("foo")
+
+b_list
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 # Dictionary
