471aaa925bd75f8cac1cf34a2b1c27d3ee963488
HeartlandNetwork <gareth_rowell@nps.gov>
2024-09-16 12:02:59 -0500

    generators

--- src/Ch3-Data-structures-functions-files.py
+++ src/Ch3-Data-structures-functions-files.py
@@ -877,22 +877,81 @@ some_dict = {"a": 1, "b": 2, "c": 3}
 
 for key in some_dict:
     print(key)
-    
-a
-b
-c
-
+   
     
 dict_iterator = iter(some_dict)
 dict_iterator
 
 list(dict_iterator)
 
+# To create a generator, use the "yield" keyword
+
 def squares(n=10):
-    print("Generating sqouares from 1 to {n ** 2}")
+    print("Generating squares from 1 to {n ** 2}")
+    for i in range(1, n + 1):
+     yield i ** 2
+     
+# when you call the generator, no code is immediately executed
+# its not until you request the elements
+     
+gen = squares()
+
+gen
+ 
+for x in gen:
+    print(x,end = " ")
+     
+     
+# generator expressions
+# analog to list, dict and set comprehensions
+
+gen = (x ** 2 for  x in range(100))
+
+gen
+
+# This is equivalent to 
+
+def _make_gen():
+    for x in range(100):
+        yield x ** 2
+gen2 = _make_gen()
+
+gen2
+
+# using a generator instead of list comprehension
+# as a function argument
+
+sum(x ** 2 for  x in range(100))
+
+dict((i , i** 2) for i in range(5))
+
+# generators can sometimes be faster than comprehension expressions
+
+
+# itertools module
+# collection of iterators for various data algorithms
+
+# example - groupby takes any sequence and function
+#    grouping consecutive elements in sequence by 
+#    return value of function
+
+import itertools
+
+def first_letter(x):
+    return x[0]
     
+names = ["Alan", "Adam", "Wes", "Will", "Albert", "Steven"]
+
+for letter, names in itertools.groupby(names, first_letter):
+    print(letter, list(names)) # names in a generator
+
+
+
     
-    
+
+
+
+
 
 
 
