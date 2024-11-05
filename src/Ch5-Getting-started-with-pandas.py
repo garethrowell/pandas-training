a01c9e4b9c347e3282c99a561ffcf4df678f7e09
HeartlandNetwork <gareth_rowell@nps.gov>
2024-11-05 11:33:57 -0600

    Arithmetic and Data Alignment

--- src/Ch5-Getting-started-with-pandas.py
+++ src/Ch5-Getting-started-with-pandas.py
@@ -606,9 +606,34 @@ data
 
 
 
-
 # Arithmetic and Data Alignment
 # -----------------------------
+
+# What happens when we union two objects
+# with different indexes?
+
+# Looking at Series
+
+s1 = pd.Series([7.3, -2.5, 3.4, 1.5], index=["a", "c", "d", "e"])
+
+s2 = pd.Series([-2.1, 3.6, -1.5, 4, 3.1], index=["a", "c", "e", "f", "g"])
+
+s1
+
+s2
+
+# Missing values generated where label locations don't overlap
+
+s1 + s2
+
+
+# Looking at DataFrame
+
+df1 = pd.DataFrame(np.arange(9.).reshape((3.3)), columns=list("bcd"),
+                   u=index=["Ohio", "Texas", "Colorado"])
+                   
+                   
+
 
 # Function Application and Mapping
 # --------------------------------
