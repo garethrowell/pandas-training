f620bd0ba591b69eef070c967c19f879b7eabaa1
garethrowell <gar417@gmail.com>
2025-03-10 10:32:05 -0500

    file parsing

--- Ch6-Data-loading-storage-file-formats.py
+++ Ch6-Data-loading-storage-file-formats.py
@@ -17,6 +17,54 @@ from pandas import Series, DataFrame
 
 # C:\Users\GRowell\pydata-book\examples
 
+# Text and binary data loading functions
+# --------------------------------------
+# read_csv, read_fwf, read_clipboard, read_excel,
+# read_hdf (hierarchical data format), read_html,
+# read_json, read_feather, read_orc (Hadoop and Spark),
+# read_parquet, read_pickle (Python), read_sas,
+# read_spss, read_sql, read_sql_table (uses SQLAlchemy),
+# read_stata (statistics software), read_xml
+
+# categories of optional arguments
+# Indexing, Type inference and data conversion,
+# Date and time parsing, Iterating, Unclean data issues
+
+# Parsing functions 
+
+# sample data:
+#(pydata-book) C:\Users\GRowell\pydata-book\examples>type ex4.csv
+# hey!
+# a,b,c,d,message
+# just wanted to make things more difficult for you
+# who reads CSV files with computers, anyway?
+#  1,2,3,4,hello
+#  5,6,7,8,world
+#  9,10,11,12,foo
+
+pd.read_csv("../pydata-book/examples/ex4.csv", skiprows=[0, 2, 3])
+
+# Handling missing values
+
+#(pydata-book) C:\Users\GRowell\pydata-book\examples>type ex5.csv
+#something,a,b,c,d,message
+#one,1,2,3,4,NA
+#two,5,6,,8,world
+#three,9,10,11,12,foo
+
+result = pd.read_csv("../pydata-book/examples/ex5.csv")
+
+result
+
+pd.isna(result)
+
+
+
+
+
+
+
+
 
 
 
